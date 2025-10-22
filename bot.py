c3d9057ffec2ee0bfc803f8224a5e269a9eb84ff
Daniil Lushev <zelyuk2003@gmail.com>
2025-10-23 01:44:22 +0300

    On branch main
     Initial commit
    
     Changes to be committed:
    	new file:   .idea/.gitignore
    	new file:   .idea/TinderBolt.iml
    	new file:   .idea/inspectionProfiles/profiles_settings.xml
    	new file:   .idea/misc.xml
    	new file:   .idea/modules.xml
    	new file:   __pycache__/gpt.cpython-314.pyc
    	new file:   __pycache__/util.cpython-314.pyc
    	new file:   bot.py
    	new file:   gpt.py
    	new file:   resources/images/avatar_main.jpg
    	new file:   resources/images/date.jpg
    	new file:   resources/images/date_gosling.jpg
    	new file:   resources/images/date_grande.jpg
    	new file:   resources/images/date_hardy.jpg
    	new file:   resources/images/date_robbie.jpg
    	new file:   resources/images/date_zendaya.jpg
    	new file:   resources/images/gpt.jpg
    	new file:   resources/images/main.jpg
    	new file:   resources/images/message.jpg
    	new file:   resources/images/opener.jpg
    	new file:   resources/images/profile.jpg
    	new file:   resources/messages/date.txt
    	new file:   resources/messages/gpt.txt
    	new file:   resources/messages/main.txt
    	new file:   resources/messages/message.txt
    	new file:   resources/messages/opener.txt
    	new file:   resources/messages/profile.txt
    	new file:   resources/prompts/date_gosling.txt
    	new file:   resources/prompts/date_grande.txt
    	new file:   resources/prompts/date_hardy.txt
    	new file:   resources/prompts/date_robbie.txt
    	new file:   resources/prompts/date_zendaya.txt
    	new file:   resources/prompts/gpt.txt
    	new file:   resources/prompts/main.txt
    	new file:   resources/prompts/message_date.txt
    	new file:   resources/prompts/message_next.txt
    	new file:   resources/prompts/opener.txt
    	new file:   resources/prompts/profile.txt
    	new file:   util.py

---
+++ bot.py
@@ -0,0 +1,185 @@
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, CallbackQueryHandler, CommandHandler
+
+from gpt import *
+from util import *
+
+# тут будем писать наш код :)
+async def start(update, context):
+    dialog.mode = "main"
+    text = load_message("main")
+    await send_photo(update, context, "main")
+    await send_text(update, context, text)
+    await show_main_menu(update, context, {
+        "start": "Главное меню бота",
+        "profile": "Генерация Tinder-профля 😎",
+        "opener": "Сообщение для знакомства 🥰",
+        "message": "Переписка от вашего имени 😈",
+        "date": "Переписка co звездами 🔥",
+        "gpt": "Общение c ИИ 🧠"
+    })
+
+async def gpt(update, context):
+    dialog.mode = "gpt"
+    text = load_message("gpt")
+    await send_photo(update, context, "gpt")
+    await send_text(update, context, text)
+
+async def gpt_dialog(update, context):
+    text = update.message.text
+    prompt = load_prompt("gpt")
+    my_message = await send_text(update, context, "Консерва набирает текст...")
+    answer = await chatgpt.send_question(prompt, text)
+    await my_message.edit_text(answer)
+
+async def date(update, context):
+    dialog.mode = "date"
+    text = load_message("date")
+    await send_photo(update, context, "date")
+    await send_text_buttons(update, context, text, {
+        "date_grande": "Ариана Гранде",
+        "date_robbie": "Марго Робби",
+        "date_zendaya": "Зендея",
+        "date_gosling": "Райан Гослинг",
+        "date_hardy": "Том Харди"
+    })
+
+async def date_dialog(update, context):
+    text = update.message.text
+    my_message = await send_text(update, context, "Партнёр набирает текст...")
+    answer = await chatgpt.add_message(text)
+    await my_message.edit_text(answer)
+
+async def date_button(update, context):
+    query =  update.callback_query.data
+    await update.callback_query.answer()
+    await send_photo(update, context, query)
+    await send_text(update, context, "Отличный выбор, можешь начинать диалог!")
+    prompt = load_prompt(query)
+    chatgpt.set_prompt(prompt)
+
+
+async def message(update, context):
+    dialog.mode = message
+    text = load_message("message")
+    await send_photo(update, context, "message")
+    await send_text_buttons(update, context, text, {
+        "message_next": "Следующее сообщение",
+        "message_date": "Пригласить на свидание"
+    })
+    dialog.list.clear()
+
+async def message_button(update, context):
+    query =  update.callback_query.data
+    await update.callback_query.answer()
+    prompt = load_prompt(query)
+    user_chat_history = "\n\n".join(dialog.list)
+    my_message = await send_text(update, context, "Консерва думает...")
+    answer = await chatgpt.send_question(prompt, user_chat_history)
+    await my_message.edit_text(answer)
+
+async def message_dialog(update, context):
+    text = update.message.text
+    dialog.list.append(text)
+
+async def profile(update, context):
+    dialog.mode = "profile"
+    text = load_message("profile")
+    await send_photo(update, context, "profile")
+    await send_text(update, context, text)
+    dialog.user.clear()
+    dialog.count = 0
+    await send_text(update, context, "Сколько вам лет?")
+
+async def profile_dialog(update, context):
+    text = update.message.text
+    dialog.count += 1
+
+    if dialog.count == 1:
+        dialog.user["age"] = text
+        await send_text(update, context, "Кем вы работаете?")
+    elif dialog.count == 2:
+        dialog.user["occupation"] = text
+        await send_text(update, context, "Какие у вас хобби?")
+    elif dialog.count == 3:
+        dialog.user["hobby"] = text
+        await send_text(update, context, "Что вам НЕ нравится в людях?")
+    elif dialog.count == 4:
+        dialog.user["annoys"] = text
+        await send_text(update, context, "Цели знакомства?")
+    elif dialog.count == 5:
+        dialog.user["goals"] = text
+        prompt = load_prompt("profile")
+        user_info = dialog_user_info_to_str(dialog.user)
+        my_message = await send_text(update, context, "Консерва думает...")
+        answer = await chatgpt.send_question(prompt, user_info)
+        await my_message.edit_text(answer)
+
+async def opener(update, context):
+    dialog.mode = "opener"
+    text = load_message("opener")
+    await send_photo(update, context, "opener")
+    await send_text(update, context, text)
+
+    dialog.user.clear()
+    dialog.count = 0
+
+    await send_text(update, context, "Имя девушки?")
+
+async def opener_dialog(update, context):
+    text = update.message.text
+    dialog.count += 1
+
+    if dialog.count == 1:
+        dialog.user["name"] = text
+        await send_text(update, context, "Сколько ей лет?")
+    elif dialog.count == 2:
+        dialog.user["age"] = text
+        await send_text(update, context, "Оцените её от 1 до 10:")
+    elif dialog.count == 3:
+        dialog.user["handsome"] = text
+        await send_text(update, context, "Кем она работает?")
+    elif dialog.count == 4:
+        dialog.user["occupation"] = text
+        await send_text(update, context, "Цель знакомства?")
+    elif dialog.count == 5:
+        dialog.user["goal"] = text
+        prompt = load_prompt("opener")
+        user_info = dialog_user_info_to_str(dialog.user)
+        my_message = await send_text(update, context, "Консерва думает...")
+        answer = await chatgpt.send_question(prompt, user_info)
+        await my_message.edit_text(answer)
+
+async def hello(update, context):
+    if dialog.mode == "gpt":
+        await gpt_dialog(update, context)
+    if dialog.mode == "date":
+        await date_dialog(update, context)
+    if dialog.mode == "message":
+        await message_dialog(update, context)
+    if dialog.mode == "profile":
+        await profile_dialog(update, context)
+    if dialog.mode == "opener":
+        await opener_dialog(update, context)
+
+dialog = Dialog()
+dialog.mode = None
+dialog.list = []
+dialog.count = 0
+dialog.user = {}
+
+chatgpt = ChatGptService(token="javcgk8pFZZssAv/GAaLFtpU2XRxcYwvevXZIyGFAmFZI3L06qepS/RV1vwI5WWJCzXUXxhEgBL78gsqw097AFLgJhfSJIYTGLJXuNmmC1WHnD5rLqK5bovPAMquTedVct0tMO3YKL7WnwWVBYot49YP/DsQPPKt8po+UHHV7OmqYXjjYWW2CcTundXGhGuyvJm5sNKlWWp5DZqBhLIWahLjBMGuOH0m3XMutHaIG8dtbZZqI=")
+
+app = ApplicationBuilder().token("7983982693:AAE1RaXuXiIrwJdOgdtbiD8rSrbYbfHb_XI").build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("gpt", gpt))
+app.add_handler(CommandHandler("date", date))
+app.add_handler(CommandHandler("message", message))
+app.add_handler(CommandHandler("profile", profile))
+app.add_handler(CommandHandler("opener", opener))
+
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, hello))
+
+app.add_handler(CallbackQueryHandler(date_button, pattern="^date_.*"))
+app.add_handler(CallbackQueryHandler(message_button, pattern="^message_.*"))
+app.run_polling()

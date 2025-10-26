2d721acf73e265bcff07a9feeb4bfad6fc23c8e3
Daniil Lushev <zelyuk2003@gmail.com>
2025-10-26 15:28:46 +0200

    modified:   resources/prompts/message_date.txt 	modified:   resources/prompts/message_next.txt 	modified:   resources/prompts/opener.txt

--- bot.py
+++ bot.py
@@ -167,9 +167,9 @@ dialog.list = []
 dialog.count = 0
 dialog.user = {}
 
-chatgpt = ChatGptService(token="")
+chatgpt = ChatGptService(token="javcgk8pFZZssAv/GAaLFtpU2XRxcYwvevXZIyGFAmFZI3L06qepS/RV1vwI5WWJCzXUXxhEgBL78gsqw097AFLgJhfSJIYTGLJXuNmmC1WHnD5rLqK5bovPAMquTedVct0tMO3YKL7WnwWVBYot49YP/DsQPPKt8po+UHHV7OmqYXjjYWW2CcTundXGhGuyvJm5sNKlWWp5DZqBhLIWahLjBMGuOH0m3XMutHaIG8dtbZZqI=")
 
-app = ApplicationBuilder().token("").build()
+app = ApplicationBuilder().token("7983982693:AAE1RaXuXiIrwJdOgdtbiD8rSrbYbfHb_XI").build()
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("gpt", gpt))
